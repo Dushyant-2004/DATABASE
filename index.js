b7ddcf654c05c77c30f67b55b1ac6c60073aef41
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-03-09 11:50:24 +0530

    some changes

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
+const User1 = new User({
+  name: 'John',
+  age: 25,
+  email: "jhon@gmail.com",
+});
+User1.save().then(() => console.log('User1 saved to database'));
+
+const User2 = new User({
+  name: 'Alice',
+  age: 35,
+  email: "alice@gmail.com",
+});
+
+User2
+.save()
+.then(() => console.log('User2 saved to database'))
+.catch(err => console.log(err));
+
